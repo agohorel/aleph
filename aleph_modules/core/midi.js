afa330fbd1f01687148a9cf4548cca7b6f6912d7
agohorel <alex.oddlogic@gmail.com>
2018-07-24 19:48:15 -0400

    fix issue with combining multiple control types
    and minor refactor/cleanup

--- aleph_modules/core/midi.js
+++ aleph_modules/core/midi.js
@@ -4,49 +4,42 @@ let midiInputs = easymidi.getInputs();
 console.log(midiInputs);
 
 let midiDevice = new easymidi.Input("Launch Control 1");
-let knob1 = 0, knob2 = 0, knob3 = 0, knob4 = 0, knob5 = 0, knob6 = 0, knob7 = 0, knob8 = 0;
+let cc1 = 0, cc2 = 0, cc3 = 0, cc4 = 0, cc5 = 0, cc6 = 0, cc7 = 0, cc8 = 0;
 let btn1 = 0, btn2 = 0, btn3 = 0, btn4 = 0, btn5 = 0, btn6 = 0, btn7 = 0, btn8 = 0;
 
 // type: noteon
 // channel 8
 // note: 9-12, 25-28
 midiDevice.on('noteon', (msg) => {
-	console.log(msg);
 	switch(msg.note){
 		case 9:
 			btn1 = msg.velocity;
 		break;
 		case 10:
-			btn2 = msg.velocy;		
+			btn2 = msg.velocity;		
 		break;
 		case 11:
-			btn3 = msg.velocy;
+			btn3 = msg.velocity;
 		break;
 		case 12:
-			btn4 = msg.velocy;
+			btn4 = msg.velocity;
 		break;
 		case 25:
-			btn5 = msg.velocy;
+			btn5 = msg.velocity;
 		break;
 		case 26:
-			btn6 = msg.velocy;
+			btn6 = msg.velocity;
 		break;
 		case 27:
-			btn7 = msg.velocy;
+			btn7 = msg.velocity;
 		break;
 		case 28:
-			btn8 = msg.velocy;
+			btn8 = msg.velocity;
 		break;
 	}
 	module.exports.controls = {
-		btn1,
-		btn2,
-		btn3,
-		btn4,
-		btn5,
-		btn6,
-		btn7,
-		btn8
+		cc1, cc2, cc3, cc4, cc5, cc6, cc7, cc8,
+		btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8
 	};
 });
 
@@ -56,36 +49,30 @@ midiDevice.on('noteoff', (msg) => {
 			btn1 = msg.velocity;
 		break;
 		case 10:
-			btn2 = msg.velocy;		
+			btn2 = msg.velocity;		
 		break;
 		case 11:
-			btn3 = msg.velocy;
+			btn3 = msg.velocity;
 		break;
 		case 12:
-			btn4 = msg.velocy;
+			btn4 = msg.velocity;
 		break;
 		case 25:
-			btn5 = msg.velocy;
+			btn5 = msg.velocity;
 		break;
 		case 26:
-			btn6 = msg.velocy;
+			btn6 = msg.velocity;
 		break;
 		case 27:
-			btn7 = msg.velocy;
+			btn7 = msg.velocity;
 		break;
 		case 28:
-			btn8 = msg.velocy;
+			btn8 = msg.velocity;
 		break;
 	}
 	module.exports.controls = {
-		btn1,
-		btn2,
-		btn3,
-		btn4,
-		btn5,
-		btn6,
-		btn7,
-		btn8
+		cc1, cc2, cc3, cc4, cc5, cc6, cc7, cc8,
+		btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8
 	};
 });
 
@@ -95,59 +82,39 @@ midiDevice.on('noteoff', (msg) => {
 midiDevice.on("cc", (msg) => {
 	switch (msg.controller){
 		case 21:
-			knob1 = msg.value;
+			cc1 = msg.value;
 	    break;
 	    case 22: 
-	    	knob2 = msg.value;
+	    	cc2 = msg.value;
 	    break;
 	    case 23:
-	    	knob3 = msg.value;
+	    	cc3 = msg.value;
     	break;
     	case 24:
-    		knob4 = msg.value;
+    		cc4 = msg.value;
 		break;
 		case 25:
-			knob5 = msg.value;
+			cc5 = msg.value;
 		break;
 		case 26:
-			knob6 = msg.value;
+			cc6 = msg.value;
 		break;
 		case 27:
-			knob7 = msg.value;
+			cc7 = msg.value;
 		break;
 		case 28:
-			knob8 = msg.value;
+			cc8 = msg.value;
 		break;
 	}
 	// re-export new values on update
 	module.exports.controls = {
-		knob1, 
-		knob2,
-		knob3, 
-		knob4,
-		knob5, 
-		knob6,
-		knob7, 
-		knob8
+		cc1, cc2, cc3, cc4, cc5, cc6, cc7, cc8,
+		btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8
 	};
 });
 
 // initial export of default values
 module.exports.controls = {
-	knob1, 
-	knob2,
-	knob3, 
-	knob4,
-	knob5, 
-	knob6,
-	knob7, 
-	knob8,
-	btn1,
-	btn2,
-	btn3,
-	btn4,
-	btn5,
-	btn6,
-	btn7,
-	btn8
+	cc1, cc2, cc3, cc4, cc5, cc6, cc7, cc8,
+	btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8
 };
--- aleph_modules/modes/simpleSpectrum.js
+++ aleph_modules/modes/simpleSpectrum.js
@@ -1,33 +1,9 @@
-// let opacity = 255;
-// let knob1 = 0, knob2 = 0;
+exports.run = (fft, volume, bass, mid, high, spectrum, waveform, spectralCentroid, midi) => {
+	let r = bass, g = mid, b = high;
 
-exports.run = (fft, volume, bass, mid, high, spectrum, waveform, spectralCentroid, midiControls) => {
-	let r = bass, g = mid, b = high, colorMod;
-
-	// switch (ccObj.controller){
-	// 	case 21:
-	// 		knob1 = ccObj.value; 
-	// 		colorMod = knob1;
-	// 		console.log(`\n\n knob1 = ${knob1}, knob2 = ${knob2}.`);
-	// 		console.log(`\n\n colorMod = ${colorMod}, opacity = ${opacity}`);
-	//     break;
-	//     case 22: 
-	//     	knob2 = map(ccObj.value, 0, 127, 0, 255);
-	//     	opacity = knob2;
-	//     	console.log(`\n\n knob1 = ${knob1}, knob2 = ${knob2}.`);
-	//     	console.log(`\n\n colorMod = ${colorMod}, opacity = ${opacity}`);
- //        break;
-	// }
-
-	// colorMod = ccObj.value;
-
-	// type: CC 
-	// channel 8
-	// controller: 21-28, 41-48
-
-	background(midiControls.btn1, midiControls.knob1);
+	background(midi.btn1, midi.cc1);
 	noStroke();
-	fill(r, g, b + midiControls.knob2);
+	fill(r + midi.btn2, g + midi.btn3, b + midi.btn4);
 
 	for (let i = 0; i < spectrum.length; i++){
 		let x = map(i, 0, spectrum.length, 0, width);
