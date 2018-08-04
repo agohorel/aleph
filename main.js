5bba35f77c44c397311e2c58d7b50613307412d7
agohorel <alex.oddlogic@gmail.com>
2018-08-04 14:50:45 -0400

    dereference windows on close, add proper exit behavior

--- main.js
+++ main.js
@@ -9,9 +9,28 @@ function createWindow() {
 
 	editorWindow.loadFile("./aleph_modules/core/index.html");
 	displayWindow.loadFile("./aleph_modules/core/displayWindow.html");
+
+	// dereference windows on close
+	editorWindow.on("closed", () => {
+	      editorWindow = null;
+	});
+
+	displayWindow.on("closed", () => {
+		displayWindow = null;
+	});
+
 }
 
 app.on("ready", createWindow);
+
+// quit when all windows are closed.
+app.on('window-all-closed', () => {
+	// On macOS it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin') {
+	  app.quit();
+	}		
+});
 
 ipcMain.on("changeMode", (event, args) => {
 	displayWindow.webContents.send("modeSelector", args);
