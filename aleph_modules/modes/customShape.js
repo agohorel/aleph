e8ab73d3f09d82c1b69f0966b52679469aadf868
agohorel <alex.oddlogic@gmail.com>
2018-08-30 18:06:53 -0400

    update customShape.js

--- aleph_modules/modes/customShape.js
+++ aleph_modules/modes/customShape.js
@@ -4,46 +4,86 @@ exports.run = (fft, volume, bass, mid, high, spectrum, waveform, spectralCentroi
 	rect(-width/2, -height/2, width, height);
 
 	strokeWeight(map(volume, 0, 1, 1, 5));	
+	scale(map(volume, 0, 1, .25, 1.25));
+	// scale(map(midi.controller(5).value, 0, 127, .1, 2));
 
 	push();
 	rotateZ(radians(45));
 
-	colors(0, 255); // invert colors each time to increase visibility
-	renderShape(TRIANGLE_STRIP, 0, 128); // start with low spectrum resolution and double each time
 	colors(255, 0);
-	renderShape(TRIANGLE_STRIP, 0, 64);	
+	renderShape(TRIANGLE_FAN, 128, -500); 
 	colors(0, 255);
-	renderShape(TRIANGLE_STRIP, 0, 32);
+	renderShape(TRIANGLE_FAN, 64, -500);	
 	colors(255, 0);
-	renderShape(TRIANGLE_STRIP, 0, 16);
+	renderShape(TRIANGLE_FAN, 32, -500);
+	colors(0, 255);
+	renderShape(TRIANGLE_FAN, 16, -500);
+
+
+	colors(255, 0); 
+	renderShapeInverse(TRIANGLE_FAN, 128, -500); 
+	colors(0, 255);
+	renderShapeInverse(TRIANGLE_FAN, 64, -500);	
+	colors(255, 0);
+	renderShapeInverse(TRIANGLE_FAN, 32, -500);
+	colors(0, 255);
+	renderShapeInverse(TRIANGLE_FAN, 16, -500);
+
+	colors(255, 0);
+	renderShape(TRIANGLE_FAN, 128, 500); 
+	colors(0, 255);
+	renderShape(TRIANGLE_FAN, 64, 500);	
+	colors(255, 0);
+	renderShape(TRIANGLE_FAN, 32, 500);
+	colors(0, 255);
+	renderShape(TRIANGLE_FAN, 16, 500);
+
+
+	colors(255, 0); 
+	renderShapeInverse(TRIANGLE_FAN, 128, 500); 
+	colors(0, 255);
+	renderShapeInverse(TRIANGLE_FAN, 64, 500);	
+	colors(255, 0);
+	renderShapeInverse(TRIANGLE_FAN, 32, 500);
+	colors(0, 255);
+	renderShapeInverse(TRIANGLE_FAN, 16, 500);
+
+	colors(0, 255); 
+	renderShape(TRIANGLE_STRIP, 128, 0); 
+	colors(255, 0);
+	renderShape(TRIANGLE_STRIP, 64, 0);	
+	colors(0, 255);
+	renderShape(TRIANGLE_STRIP, 32, 0);
+	colors(255, 0);
+	renderShape(TRIANGLE_STRIP, 16, 0);
 
 
 	colors(0, 255); 
-	renderShapeInverse(TRIANGLE_STRIP, 0, 128); 
+	renderShapeInverse(TRIANGLE_STRIP, 128, 0); 
 	colors(255, 0);
-	renderShapeInverse(TRIANGLE_STRIP, 0, 64);	
+	renderShapeInverse(TRIANGLE_STRIP, 64, 0);	
 	colors(0, 255);
-	renderShapeInverse(TRIANGLE_STRIP, 0, 32);
+	renderShapeInverse(TRIANGLE_STRIP, 32, 0);
 	colors(255, 0);
-	renderShapeInverse(TRIANGLE_STRIP, 0, 16);
+	renderShapeInverse(TRIANGLE_STRIP, 16, 0);
 
 	pop();
 }
 
-function renderShape(shapeType, start, stepSize){
+function renderShape(shapeType, stepSize, offset){
 	beginShape(shapeType);
-	for (let i = start; i < spectrum.length; i+=stepSize){
-		let x = map(i, 0, spectrum.length, -width/2, width/2);
+	for (let i = 0; i < spectrum.length; i+=stepSize){
+		let x = map(i, 0, spectrum.length, -width/2 + offset, width/2 + offset);
 	    let h = map(spectrum[i], 0, 255, height/2, -height/2);
 		vertex(x, h);
 	}
 	endShape();
 }
 
-function renderShapeInverse(shapeType, start, stepSize){
+function renderShapeInverse(shapeType, stepSize, offset){
 	beginShape(shapeType);
-	for (let i = start; i < spectrum.length; i+=stepSize){
-		let x = map(i, 0, spectrum.length, -width/2, width/2);
+	for (let i = 0; i < spectrum.length; i+=stepSize){
+		let x = map(i, 0, spectrum.length, -width/2 + offset, width/2 + offset);
 	    let h = map(spectrum[i], 0, 255, height/2, -height/2);
 		vertex(h, x);
 	}
