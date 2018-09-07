565076dde46d2a57659f688f14c3c022b8a421d0
agohorel <alex.oddlogic@gmail.com>
2018-09-07 14:06:20 -0400

    visual improvements to 3D.js
    -changed to specular material
    -added colored point lights
    -improved colors
    -removed stroke
    -added constant Z axis rotation
    -increased easing
    -fixed over the top initialization easing madness

--- aleph_modules/modes/3D.js
+++ aleph_modules/modes/3D.js
@@ -1,4 +1,4 @@
-let leftVolEased = 1, rightVolEased = 1, easing = 0.05; 
+let leftVolEased = .001, rightVolEased = .001, easing = 0.025; 
 let scale = .1;
 
 exports.run = (fft, volume, bass, mid, high, spectrum, waveform, spectralCentroid, midi, leftVol, rightVol) => {
@@ -15,16 +15,17 @@ exports.run = (fft, volume, bass, mid, high, spectrum, waveform, spectralCentroi
 	let dirX = (mouseX / width - 0.5) * 2;
 	let dirY = (mouseY / height - 0.5) * 2;
 
-	directionalLight(bass, mid, high, 1.5, .75, 0.25);
-	directionalLight(bass - 50, mid - 50, high - 50, 0, .4, 0.25);
-	ambientMaterial(250);
+	directionalLight(bass, mid * .5, high, 1.5, .75, 0.25);
+	directionalLight(bass - 50, mid * .5 - 50, high - 50, 0, .4, 0.25);
+	pointLight(bass/2, 0, high * 5, -225, -115, 200);
+	pointLight(0, 0, high * 5, 225, height - 115, 200);	
+
+	specularMaterial(255);
 
 	rotateX(frameCount * leftVolEased * .5);
-	rotateY(frameCount * rightVolEased * .5);
-	
-	stroke(255);
-	strokeWeight(map(volume, 0, 1, 1, 5));
+	rotateY(frameCount * rightVolEased * .5);	
+	rotateZ(frameCount * .01);	
 
-	let size = leftVolEased + rightVolEased * 30000;
+	let size = leftVolEased + rightVolEased * 20000;
 	box(size);
 }
