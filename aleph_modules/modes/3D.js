894830116688bdbf4a311f64381591f9545a77ea
agohorel <alex.oddlogic@gmail.com>
2018-09-07 18:33:24 -0400

    update variable name to prevent collision with p5 scale function

--- aleph_modules/modes/3D.js
+++ aleph_modules/modes/3D.js
@@ -1,19 +1,16 @@
 let leftVolEased = .001, rightVolEased = .001, easing = 0.025; 
-let scale = .1;
+let scaler = .1;
 
 exports.run = (fft, volume, bass, mid, high, spectrum, waveform, spectralCentroid, midi, leftVol, rightVol) => {
 	background(0);
-	
-	let targetL = leftVol * scale;
+
+	let targetL = leftVol * scaler;
 	let distL = targetL - leftVolEased;
 	leftVolEased += distL * easing;
 
-	let targetR = rightVol * scale;
+	let targetR = rightVol * scaler;
 	let distR = targetR - rightVolEased;
 	rightVolEased += distR * easing;
-
-	let dirX = (mouseX / width - 0.5) * 2;
-	let dirY = (mouseY / height - 0.5) * 2;
 
 	directionalLight(bass, mid * .5, high, 1.5, .75, 0.25);
 	directionalLight(bass - 50, mid * .5 - 50, high - 50, 0, .4, 0.25);
