a234ffcc70395981e12e573e1bc683e296372a7c
agohorel <alex.oddlogic@gmail.com>
2018-09-07 18:34:45 -0400

    add loadModel.js example file
    need to figure out why the rotation origin seems to be different when loading 3d mesh off of an object.

---
+++ aleph_modules/modes/loadModel.js
@@ -0,0 +1,26 @@
+let leftVolEased = .001, rightVolEased = .001, easing = 0.025; 
+let scaler = .1;
+
+exports.run = (fft, volume, bass, mid, high, spectrum, waveform, spectralCentroid, midi, leftVol, rightVol, models) => {
+	background(0);
+
+	let targetL = leftVol * scaler;
+	let distL = targetL - leftVolEased;
+	leftVolEased += distL * easing;
+
+	let targetR = rightVol * scaler;
+	let distR = targetR - rightVolEased;
+	rightVolEased += distR * easing;
+
+	let volumeEased = leftVolEased + rightVolEased * .5;
+
+	rotateX(frameCount * leftVolEased * .5);
+	rotateZ(frameCount * rightVolEased * .5);
+	rotateY(frameCount * 0.01);
+	
+
+	stroke(map(volumeEased, 0, .025, 0, 255));
+	fill(bass * .2, mid, high * 2);
+	scale(volumeEased * 100, volumeEased * 100, volumeEased * 100);
+	model(models.floppy);
+}
