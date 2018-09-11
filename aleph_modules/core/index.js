agohorel/aleph

commit 214d0aa31027b14d696ce60b27f5364f7feb462b

makeDomElement function supports arbitrary no. of classes
put class names inside an array when invoking the function (even if using a single class!)

aleph_modules/core/index.js
@@ -13,7 +13,7 @@ fs.readdir("./aleph_modules/modes", (err, files) => {
   	console.log(err);
   } else {
   	  files.forEach(file => {
-  	  	makeDomElement("BUTTON", file.substring(0, file.length-3), "modeSelectButton", "#modeSelectorButtons", true);	  
+  	  	makeDomElement("BUTTON", file.substring(0, file.length-3), ["modeSelectButton", "btn"], "#modeSelectorButtons", true);	  
 	  });
   }
 });
@@ -33,7 +33,7 @@ const midiDeviceButtons = document.querySelector("#midiDeviceButtons");
 // display available midi devices once they have been sent from main process
 ipc.on("displayMidi", (event, arg) => {
 	for (let i = 0; i < arg.length; i++){
-		makeDomElement("BUTTON", arg[i], "midiDeviceButtons","#midiDeviceButtons", true);                                    
+		makeDomElement("BUTTON", arg[i], ["midiDeviceButtons", "btn"],"#midiDeviceButtons", true);                                    
 	}
 });
 
@@ -57,7 +57,7 @@ let controlCount = 0;
 // create new midi control mappings
 addMidiMap.addEventListener("click", () => {	
 	controlCount++;
-	makeDomElement("BUTTON", controlCount, "midiMapping", "#midiMapIcons", false);
+	makeDomElement("BUTTON", controlCount, ["midiMapping", "btn"], "#midiMapIcons", false);
 });
 
 // highlight selected midi control mapping slot & send controller id to main process
@@ -141,7 +141,11 @@ function makeDomElement(type, text, className, destParent, boolean) {
 	let element = document.createElement(type);
 	let displayText = document.createTextNode(text);
 	element.appendChild(displayText);
-	element.classList.add(className);
+
+	for (let i = 0; i < className.length; i++){
+		element.classList.add(className[i]);
+	}
+	
 	element.id = text;
 	element.disabled = boolean;
 	document.querySelector(destParent).appendChild(element);

commit e92e6d79e288cc12c54a368579243945c6bf9832

fix event listener filtering broken by commit 214d0aa
the existence of multiple classes broke my condition for filtering the mode selection buttons. using .includes() instead of an equivalence test resolves this issue.

aleph_modules/core/index.js
@@ -20,7 +20,7 @@ fs.readdir("./aleph_modules/modes", (err, files) => {
 
 // highlight selected mode & send mode to p5 via main process
 modeSelectorButtons.addEventListener("click", function(e) {
-	if (e.target.className === "modeSelectButton"){
+	if (e.target.className.includes("modeSelectButton")){
 		highlightSelectedItem(".modeSelectButton", e.target);
 		ipc.send("changeMode", e.target.id);
 	}