6b7ff12c116fbbbcd1b0b5293538291fdcba1fe7
agohorel <alex.oddlogic@gmail.com>
2018-09-13 18:57:02 -0400

    add dynamic texturing to loadModel.js, example texture files

--- aleph_modules/modes/loadModel.js
+++ aleph_modules/modes/loadModel.js
@@ -1,9 +1,10 @@
 let leftVolEased = .001, rightVolEased = .001, easing = 0.025; 
 let scaler = .1;
+let textures = [];
 
 exports.run = (fft, volume, bass, mid, high, spectrum, waveform, spectralCentroid, midi, leftVol, rightVol, assets) => {
 	background(255);
-
+ 
 	let targetL = leftVol * scaler;
 	let distL = targetL - leftVolEased;
 	leftVolEased += distL * easing;
@@ -13,14 +14,27 @@ exports.run = (fft, volume, bass, mid, high, spectrum, waveform, spectralCentroi
 	rightVolEased += distR * easing;
 
 	let volumeEased = leftVolEased + rightVolEased * .5;
-
-	rotateX(frameCount * leftVolEased * .5);
-	rotateZ(frameCount * rightVolEased * .5);
+	
 	rotateY(frameCount * 0.01);
+	scale(2, -2);
 
-	stroke(map(volumeEased, 0, .025, 0, 255));
-	fill(bass * .1, mid * .5, high * 255);
+	stroke(map(volumeEased, 0, .025, 255, 0));
+	fill(map(volume, 0, 1, 0, 255));
 
-	texture(assets.textures.warped);
+	// check if we've already packed the array so we don't keep adding to it
+	if (textures.length < Object.keys(assets.textures).length){
+		Object.keys(assets.textures).forEach((texture) => {
+			// add each texture into the textures array so we can pull them at random
+			textures.push(assets.textures[texture]);
+		});
+	}
+
+	if (volume > .2){
+		// randomly change the texture when the volume hits a certain threshold 
+		// this is to illustrate how there's no latency when swapping textures
+		// because all imported assets are preloaded into RAM on launch
+		texture(textures[Math.floor(Math.random() * textures.length)]);
+	}
+
 	model(assets.models.floppy);
 }
