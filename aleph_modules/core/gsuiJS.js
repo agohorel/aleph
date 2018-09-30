c4caf0f70b7bbb0f3e38294065ac5bd06ccb480a
agohorel <alex.oddlogic@gmail.com>
2018-09-29 21:36:30 -0400

    gs-ui test
    implementing their codepen demo here: https://codepen.io/gridsound/pen/NMGqOQ

---
+++ aleph_modules/core/gsuiJS.js
@@ -0,0 +1,20 @@
+const s = [
+	new gsuiSlider(),
+	new gsuiSlider(),
+	new gsuiSlider(),
+	new gsuiSlider(),
+	new gsuiSlider(),
+	new gsuiSlider()
+];
+
+s[ 0 ].options( { type: "linear-y", min:  0, max: 100, step: 0.001,                 startFrom: 0, value: 33    } );
+s[ 1 ].options( { type: "linear-x", min: -1, max:   1, step: 0.001, scrollStep: .1, startFrom: 0, value:  0.33 } );
+s[ 2 ].options( { type: "linear-x", min: -1, max:   1, step: 0.2,   scrollStep: .1, startFrom: 0, value: -0.66 } );
+s[ 3 ].options( { type: "circular", min:  0, max: 100, step: 10,                    startFrom: 0, value: 33    } );
+s[ 4 ].options( { type: "circular", min:  0, max: 100, step: 0.001, scrollStep: 10, startFrom: 0, value: 66    } );
+s[ 5 ].options( { type: "circular", min: -1, max:   1, step: 0.001, scrollStep: .1, startFrom: 0, value: -0.33 } );
+
+s.forEach( ( slider, i ) => {
+	document.querySelector( "#sliderWrap" + i ).append( slider.rootElement );
+	slider.attached();
+} );
