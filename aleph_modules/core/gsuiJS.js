24a951ea54067bcd99e3d6111b1f3340c53a7148
agohorel <alex.oddlogic@gmail.com>
2018-09-30 12:01:13 -0400

    set up knob ranges/behavior

--- aleph_modules/core/gsuiJS.js
+++ aleph_modules/core/gsuiJS.js
@@ -7,12 +7,18 @@ const s = [
 	new gsuiSlider()
 ];
 
-s[ 0 ].options( { type: "circular", min:  0, max: 100, step: 10,                    startFrom: 0, value: 33    } );
-s[ 1 ].options( { type: "circular", min:  0, max: 100, step: 0.001, scrollStep: 10, startFrom: 0, value: 66    } );
-s[ 2 ].options( { type: "circular", min: -1, max:   1, step: 0.001, scrollStep: .1, startFrom: 0, value: -0.33 } );
-s[ 3 ].options( { type: "circular", min:  0, max: 100, step: 10,                    startFrom: 0, value: 33    } );
-s[ 4 ].options( { type: "circular", min:  0, max: 100, step: 0.001, scrollStep: 10, startFrom: 0, value: 66    } );
-s[ 5 ].options( { type: "circular", min: -1, max:   1, step: 0.001, scrollStep: .1, startFrom: 0, value: -0.33 } );
+// volume 
+s[ 0 ].options( { type: "circular", min:  0, max: 2, step: 0.001, startFrom: 0, value: 1      } );
+// bass
+s[ 1 ].options( { type: "circular", min:  0, max: 2, step: 0.001, startFrom: 0, value: 1      } );
+// mid
+s[ 2 ].options( { type: "circular", min: 0, max:  2, step: 0.001, startFrom: 0, value: 1      } );
+// high
+s[ 3 ].options( { type: "circular", min:  0, max: 2, step: 0.001, startFrom: 0, value: 1      } );
+// fft smoothing
+s[ 4 ].options( { type: "circular", min:  0, max: 2, step: 0.001, startFrom: 0, value: 1      } );
+// volume smoothing
+s[ 5 ].options( { type: "circular", min: .001, max:.5, step: 0.001, startFrom: 0, value: 0.25 } );
 
 s.forEach( ( slider, i ) => {
 	document.querySelector( "#sliderWrap" + i ).append( slider.rootElement );
