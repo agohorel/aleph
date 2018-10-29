d6f9a2412f866629dae5f765405cb8a6cce4c658
agohorel <alex.oddlogic@gmail.com>
2018-10-29 18:00:53 -0400

    fix/improve perf of spectralCentroid.js example file

--- aleph_modules/sketches/spectralCentroid.js
+++ aleph_modules/sketches/spectralCentroid.js
@@ -2,6 +2,7 @@ let centroidplot = 0.0;
 let spectralCentroid = 0;
 
 exports.run = (audio, midi, assets) => {
+	console.log(frameRate());
 	background(0);
 	stroke(0,255,0);
 	strokeWeight(1);
@@ -10,14 +11,17 @@ exports.run = (audio, midi, assets) => {
 	translate(-width/2, -height/2, 0);
 
 	//draw the spectrum
-	for (let i = 0; i< audio.spectrum.length; i++){
-	  let x = map(log(i), 0, log(audio.spectrum.length), 0, width);
-	  let h = map(audio.spectrum[i], 0, 255, 0, height);
-	  let rectangle_width = (log(i+1)-log(i))*(width/log(audio.spectrum.length));
-	  rect(x, height, rectangle_width, -h )
+	for (let i = 0; i < audio.spectrum.length; i+=2){
+		// grab x coord and scale it based on screen width using p5's map()
+		let x = map(i, 0, audio.spectrum.length, 0, width);
+		// grab x coord and scale it based on screen height using p5's map()
+	    let h = -height + map(audio.spectrum[i], 0, 255, height, 0);
+	    // draw a rectangle for each slice of the spectrum at x, h coords
+	    rect(x, height, width / audio.spectrum.length, h);
 	}
 
 	let nyquist = 22050;
+	spectralCentroid = audio.spectralCentroid;
 
 	// the mean_freq_index calculation is for the display.
 	let mean_freq_index = spectralCentroid/(nyquist/audio.spectrum.length);
@@ -25,4 +29,5 @@ exports.run = (audio, midi, assets) => {
 	centroidplot = map(log(mean_freq_index), 0, log(audio.spectrum.length), 0, width);
 
 	stroke(255,0,0); // the line showing where the centroid is will be red
+	rect(centroidplot, 0, width / spectrum.length, height);
 }
