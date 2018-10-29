a7de79f01fe92d83b21859af29a0cfe69928af77
agohorel <alex.oddlogic@gmail.com>
2018-10-29 16:49:30 -0400

    add comments to simpleSpectrum.js example file

--- aleph_modules/sketches/simpleSpectrum.js
+++ aleph_modules/sketches/simpleSpectrum.js
@@ -1,15 +1,23 @@
 exports.run = (audio, midi, assets) => {
+	// create color variables based on frequency content 
 	let r = audio.bass, g = audio.mid, b = audio.high;
 
+	// set black background
 	background(0);
+	// remove outlines
 	noStroke();
+	// set fill color to our color-coded variables from above
 	fill(r, g, b);
+	// transate 0, 0 point (origin) to top left corner of screen
 	translate(-width/2, -height/2, 0);
 
+	// loop through the audio.spectrum array one slice at a time 
 	for (let i = 0; i < audio.spectrum.length; i++){
+		// grab x coord and scale it based on screen width using p5's map()
 		let x = map(i, 0, audio.spectrum.length, 0, width);
+		// grab x coord and scale it based on screen height using p5's map()
 	    let h = -height + map(audio.spectrum[i], 0, 255, height, 0);
+	    // draw a rectangle for each slice of the spectrum at x, h coords
 	    rect(x, height, width / audio.spectrum.length, h);
 	}
-
 }
