6db5399957d689f1db186e1a94c534e37337df78
agohorel <alex.oddlogic@gmail.com>
2018-10-29 21:47:41 -0400

    add comments to ctrlTest.js example file

--- aleph_modules/sketches/ctrlTest.js
+++ aleph_modules/sketches/ctrlTest.js
@@ -1,19 +1,20 @@
 exports.run = (audio, midi, assets) => {
-	console.log(midi);
-
+	// link RGB variables to first 3 midi controls & remap from 0-127 to 0-255
 	let r = map(midi[0].value, 0, 127, 0, 255);
 	let g = map(midi[1].value, 0, 127, 0, 255);
 	let b = map(midi[2].value, 0, 127, 0, 255);	
-
-
+	// link brightness variable to fourth midi control
 	let brightness = midi[3].value;
-
+	// translate 0,0 coord to top left 
 	translate(-width/2, -height/2);
+	// change rectMode so the origin is in the center of the rect
 	rectMode(CENTER);
-	
+	// set background to the color determined by midi controls 1-3
 	background(r, g, b);
-
+	// set fill to the color described above, modified by the brightness variable
 	fill(r + brightness, g + brightness, b + brightness);
+	// remove outlines
 	noStroke();
+	// draw square in the center, colored by the above fill()
 	rect(width/2, height/2, width/3, width/3);
 }
