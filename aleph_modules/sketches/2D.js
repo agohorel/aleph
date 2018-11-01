5ccfe21751c46682c59547d280ddd819905f1509
agohorel <alex.oddlogic@gmail.com>
2018-11-01 13:53:02 -0400

    update 2D.js example file  to spectrum

--- aleph_modules/sketches/2D.js
+++ aleph_modules/sketches/2D.js
@@ -1,8 +1,20 @@
 exports.run = (audio, midi, assets) => {
-	_2D.rectMode(CENTER);
-	_2D.fill(255, 0, 0);
-	_2D.background(0, 100, 0);
-	_2D.rect(0, 0, 200, 200);
+	let r = audio.bass, g = audio.mid, b = audio.high;
+
+	_2D.background(0);
+	_2D.noStroke();
+	_2D.fill(r, g, b);
+
+	for (let i = 0; i < audio.spectrum.length; i++){
+		let x = map(i, 0, audio.spectrum.length, 0, width);
+	    let h = -height + map(audio.spectrum[i], 0, 255, height, 0);
+	    _2D.rect(x, height, width / audio.spectrum.length, h);
+	}
+	
+	// _2D.rectMode(CENTER);
+	// _2D.fill(255, 0, 0);
+	// _2D.background(0, 100, 0);
+	// _2D.rect(mouseX, mouseY, 200, 200);
 	texture(_2D);
-	plane(width, height);
+	plane(windowWidth, windowHeight);
 }
