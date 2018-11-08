a8fae1ac75d94a653f28d11469a033e61755feb2
agohorel <alex.oddlogic@gmail.com>
2018-11-08 18:48:52 -0500

    improved fix for _2D resizing
    declare _2D.size once up top and link all size related things to _2D.width/height instead of width/height or displayWidth/Height.

--- aleph_modules/sketches/2D.js
+++ aleph_modules/sketches/2D.js
@@ -1,18 +1,18 @@
 exports.run = (audio, midi, assets) => {
 	let r = audio.bass, g = audio.mid, b = audio.high;
-
+	
 	_2D.size(windowWidth, windowHeight);
-	_2D.background(0);
+	_2D.background(255 - volume * 255);
 	_2D.noStroke();
 	_2D.fill(r, g, b);
 
 	for (let i = 0; i < audio.spectrum.length; i++){
 		let x = map(i, 0, audio.spectrum.length, 0, width);
 	    let h = -height + map(audio.spectrum[i], 0, 255, height, 0);
-	    _2D.rect(x, height, width / audio.spectrum.length, h);
+	    _2D.rect(x, _2D.height, _2D.width / audio.spectrum.length, h);
 	}
 
-	background(0);
+	background(255);
 	texture(_2D);
-	plane(windowWidth, windowHeight);
+	plane(_2D.width, _2D.height);
 }
