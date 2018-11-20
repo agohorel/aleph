6578e2cce674dfe05353102194ed58479e5665ab
agohorel <alex.oddlogic@gmail.com>
2018-11-20 16:16:46 -0500

    add basic video importer

--- aleph_modules/core/js/p5_handler.js
+++ aleph_modules/core/js/p5_handler.js
@@ -13,7 +13,7 @@ const assetsPath = path.resolve(appPath, "aleph_modules/assets");
 const sketchesPath = path.resolve(appPath, "aleph_modules/sketches");
 
 let fft, input, spectrum, waveform, spectralCentroid, bass, mid, high, moduleName = "", amplitude, leftVol, rightVol, leftVolEased = .001, rightVolEased = .001, volEased = .001;
-let assets = {models: {}, textures: {}, fonts: {}, shaders: {}};
+let assets = {models: {}, textures: {}, fonts: {}, shaders: {}, videos: {}};
 let audio = {};
 let audioParams = {0: 1, 1: 1, 2: 1, 3: 1, 4: .45, 5: 0.25}; // set up initial values for audioParams object
 let cnv, _2D;
@@ -23,6 +23,7 @@ function preload() {
 	importer("textures");
 	importer("fonts");
 	importer("shaders");
+	importer("videos");
 }
 
 function setup() {
@@ -113,7 +114,7 @@ function importer(folder){
 
 			files.forEach((file, index) => {
 				// get file name
-				let name = file.substring(0, file.lastIndexOf(".")) || file;
+				let name = file.substring(0, file.lastIndexOf(".")).replace(/[- ]/g, "_") || file;
 
 				// check which folder we're importing from 
 				if (folder === "models"){
@@ -132,6 +133,11 @@ function importer(folder){
 					if (file.substring(file.length-4, file.length) !== ".txt"){
 						assets.fonts[fontName] = loadFont(path.join(assetsPath, "fonts", file));;
 					}
+				}
+
+				if (folder === "videos"){
+					assets.videos[name] = createVideo(path.join(assetsPath, "videos", file));
+					assets.videos[name].hide();
 				}
 				
 			});			
