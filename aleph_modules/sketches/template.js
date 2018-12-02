4379f3dd02a04c64aace20f9a3d35ce017b96a83
agohorel <alex.oddlogic@gmail.com>
2018-12-01 21:42:36 -0500

    rough 3D template
    i think this can still be cleaned up/abstracted away more. ideal would just be runOnce() and myCode() to mimic setup() and draw() of p5

---
+++ aleph_modules/sketches/template.js
@@ -0,0 +1,29 @@
+let hasRun = false;
+let r;
+
+function runOnce(){
+	let sketch = utils.getSketchName(__filename);
+	r = renderers[sketch];
+
+	// put code you only want to run once here. think of it like a second setup(). kinda. 
+}
+
+exports.run = (audio, midi, assets, utils) => {
+	utils.runOnce(hasRun, runOnce);
+	update3D(r, myCode);
+	image(r, 0, 0, width, height);
+}
+
+function myCode(){
+	r.background(200);
+	r.normalMaterial();
+	r.rotateX(frameCount * 0.01);
+	r.box(50);
+}
+
+function update3D(renderer, yourCode){
+	renderer.push();
+	yourCode();
+	renderer._renderer._update();
+	renderer.pop();
+}
