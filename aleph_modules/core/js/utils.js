88320cd123915f7d9e59ee67020ce50866dae6b9
agohorel <alex.oddlogic@gmail.com>
2019-06-19 21:55:55 -0400

    fix transforms not resetting

--- aleph_modules/core/js/utils.js
+++ aleph_modules/core/js/utils.js
@@ -14,14 +14,13 @@ exports.getSketchName = (myPath) => {
 exports.update3D = (renderer, yourCode) => {
 	renderer.push();
 	yourCode();
-	renderer._renderer._update();
+	// renderer._renderer._update();
 	renderer.pop();
 }
 
 exports.renderLoop = (hasRun, setup, renderer, draw) => {
 	module.exports.runOnce(hasRun, setup);
-	// module.exports.update3D(renderer, draw);
-	draw();
+	module.exports.update3D(renderer, draw);
 	image(renderer, 0, 0, width, height);
 }
 
